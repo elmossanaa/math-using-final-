b4aa3339f6ece800a03005d4bb0cc588877dad48
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2022-02-21 14:59:13 -0500

    Create math_with_PI_and_Pow.java

---
+++ math_with_PI_and_Pow.java
@@ -0,0 +1,8 @@
+public class math_with_PI_and_Pow {
+
+    public static final double num = Math.PI;
+    public static void main(String[] args) {
+        double c = 3.399;
+        double d = Math.pow(c,num);
+        System.out.print(d);
+        
